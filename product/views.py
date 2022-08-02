a3b102c8fb887e16d751be6002e7cdc4c2afc782
lokki <44494883+luckylokki@users.noreply.github.com>
2022-08-02 14:18:41 +0300

    task 4 done+

--- product/views.py
+++ product/views.py
@@ -3,31 +3,34 @@ from django.shortcuts import render
 
 product_catalog = [
     {
+        "id": 1,
         "product": "Iphone 11",
         "slug": "iphone-11-128",
         "memory": "128gb",
         "available": True,
         "image": "https://www.tradeinn.com/f/13735/137354156/apple-iphone-11-128gb-6.1.jpg",
         "description": "Description for apple product",
-        "price": 22000,
+        "price": 22_000.00,
     },
     {
+        "id": 2,
         "product": "Iphone 12",
         "slug": "iphone-12-128",
         "memory": "128gb",
         "available": False,
         "image": "https://www.tradeinn.com/f/13782/137821875/apple-iphone-12-4gb-128gb-6.1.jpg",
         "description": "Description for apple product",
-        "price": 43000
+        "price": 43_000.00
     },
     {
+        "id": 3,
         "product": "MacBook Pro M1",
         "slug": "macbook-pro-m1-256",
         "memory": "256gb",
         "available": True,
         "image": "https://www.tradeinn.com/f/13841/138412956/apple-macbook-pro-14-m1-pro-16gb--512gb-ssd-Ноутбук.jpg",
         "description": "Description for apple product",
-        "price": 89000
+        "price": 89_000.00
     },
 ]
 
--- profile/views.py
+++ profile/views.py
@@ -2,6 +2,7 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 user_data = {
+    'id': 1,
     'username': 'lokki',
     'first_name': 'Vadim',
     'last_name': 'Prokopenko',
