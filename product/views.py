71c9dedcc1024caf7460cd6b4805df17416cb305
lokki <44494883+luckylokki@users.noreply.github.com>
2022-08-04 10:50:50 +0300

    horizontal product list

--- product/views.py
+++ product/views.py
@@ -7,6 +7,7 @@ product_catalog = [
         "product": "Iphone 11",
         "slug": "iphone-11-128",
         "memory": "128gb",
+        "discount": "20%",
         "available": True,
         "image": "https://www.tradeinn.com/f/13735/137354156/apple-iphone-11-128gb-6.1.jpg",
         "description": "Description for apple product",
@@ -17,6 +18,7 @@ product_catalog = [
         "product": "Iphone 12",
         "slug": "iphone-12-128",
         "memory": "128gb",
+        "discount": "15%",
         "available": False,
         "image": "https://www.tradeinn.com/f/13782/137821875/apple-iphone-12-4gb-128gb-6.1.jpg",
         "description": "Description for apple product",
@@ -27,6 +29,7 @@ product_catalog = [
         "product": "MacBook Pro M1",
         "slug": "macbook-pro-m1-256",
         "memory": "256gb",
+        "discount": "10%",
         "available": True,
         "image": "https://www.tradeinn.com/f/13841/138412956/apple-macbook-pro-14-m1-pro-16gb--512gb-ssd-Ноутбук.jpg",
         "description": "Description for apple product",
@@ -45,5 +48,6 @@ def product_page(request: HttpRequest, product: str) -> HttpResponse:
 def homepage(request: HttpRequest) -> HttpResponse:
     context = []
     for item in product_catalog:
-        context.append({'product': item['product'], 'slug': item['slug'], 'image': item['image'], 'memory': item['memory'], 'available': item['available']})
+        context.append({'product': item['product'], 'slug': item['slug'], 'image': item['image'], 'memory': item['memory'],
+                        'available': item['available'], 'discount': item['discount']})
     return render(request, 'products_list.html', {'catalog': context})
