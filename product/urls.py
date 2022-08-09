be44d5fb821566fecc5965f58b459cc573e3b068
lokki <44494883+luckylokki@users.noreply.github.com>
2022-08-09 18:55:14 +0300

    worked

--- product/urls.py
+++ product/urls.py
@@ -5,5 +5,5 @@ from product.views import product_page_details, product_list_view
 
 urlpatterns = [
     path("", product_list_view),
-    path("product/<slug:slug>/", product_page_details, name="product_details")
+    path("product/<slug:slug>/", product_page_details, name="product_details"),
 ]
