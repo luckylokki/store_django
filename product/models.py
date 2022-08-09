09ce0b4d7bfbb4b05f5ee1e8ee223490394ca944
lokki <44494883+luckylokki@users.noreply.github.com>
2022-08-09 11:00:39 +0300

    modesl, sql migrate,

--- product/models.py
+++ product/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 
-
 class Category(models.Model):
     slug = models.SlugField(max_length=64)
     name = models.CharField(max_length=64)
     description = models.TextField()
-
 
 class Product(models.Model):
     slug = models.SlugField(max_length=64)
