4843498a2051c188abea29eb872569c907e16cf4
lokki <44494883+luckylokki@users.noreply.github.com>
2022-09-19 15:57:03 +0300

    Task9 done

--- product/urls.py
+++ product/urls.py
@@ -1,11 +1,16 @@
-"""User App URL Configuration"""
+'''User App URL Configuration'''
 
 from django.urls import path
-from product.views import ProductPageDetails, ProductListView, CategoryFilter, MemoryFilter
+from product.views import ProductPageDetails, ProductListView, CategoryFilter, MemoryFilter, AddProduct, UpdateProduct, DeleteProduct, AddCategory
 
 urlpatterns = [
-    path("", ProductListView.as_view(), name="product_list"),
-    path("product/<slug:slug>/", ProductPageDetails.as_view(), name="product_details"),
-    path("category/<int:category>/", CategoryFilter.as_view(), name="category_filter"),
-    path("filter/<str:memory>/", MemoryFilter.as_view(), name="memory_filter")
+    path('', ProductListView.as_view(), name='product_list'),
+    path('product/<slug:slug>/', ProductPageDetails.as_view(), name='product_details'),
+    path('category/<int:category>/', CategoryFilter.as_view(), name='category_filter'),
+    path('filter/<str:memory>/', MemoryFilter.as_view(), name='memory_filter'),
+    path('add/', AddProduct.as_view(), name='add_product'),
+    path('update/<slug:slug>/', UpdateProduct.as_view(), name='update_product'),
+    path('delete/<slug:slug>', DeleteProduct.as_view(), name='delete_product'),
+    path('add-category/', AddCategory.as_view(), name='add_category'),
+
 ]
--- product/views.py
+++ product/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from product.models import Product, Category
 
@@ -13,8 +15,8 @@ class ProductListView(ListView):
     template_name = 'product_list.html'
 
     def get_context_data(self, **kwargs):
-        context = {"product_list": Product.objects.all(), "category_list": Category.objects.all(),
-                   "memory_list": Product.objects.all().values("memory").distinct()}
+        context = {'product_list': Product.objects.all(), 'category_list': Category.objects.all(),
+                   'memory_list': Product.objects.all().values('memory').distinct()}
         return context
 
 
@@ -23,9 +25,9 @@ class CategoryFilter(ListView):
     template_name = 'product_list.html'
 
     def get_context_data(self, **kwargs):
-        context = {"product_list": Product.objects.filter(category=self.kwargs['category']),
-                   "category_list": Category.objects.all(),
-                   "memory_list": Product.objects.all().values("memory").distinct()}
+        context = {'product_list': Product.objects.filter(category=self.kwargs['category']),
+                   'category_list': Category.objects.all(),
+                   'memory_list': Product.objects.all().values('memory').distinct()}
         return context
 
 
@@ -34,8 +36,44 @@ class MemoryFilter(ListView):
     template_name = 'product_list.html'
 
     def get_context_data(self, **kwargs):
-        context = {"product_list": Product.objects.filter(memory=self.kwargs['memory']),
-                   "category_list": Category.objects.all(),
-                   "memory_list": Product.objects.all().values("memory").distinct()}
+        context = {'product_list': Product.objects.filter(memory=self.kwargs['memory']),
+                   'category_list': Category.objects.all(),
+                   'memory_list': Product.objects.all().values('memory').distinct()}
         return context
 
+class AddProduct(LoginRequiredMixin, CreateView):
+
+    http_method_names = 'get', 'post'
+    model = Product
+    fields = '__all__'
+    template_name = 'form_product.html'
+
+    def form_valid(self, form):
+        form.instance.reporter = self.request.user
+        return super().form_valid(form)
+
+class UpdateProduct(LoginRequiredMixin, UpdateView):
+
+    http_method_names = 'get', 'post'
+    model = Product
+    fields = '__all__'
+    template_name = 'form_product.html'
+
+    def form_valid(self, form):
+        form.instance.reporter = self.request.user
+        return super().form_valid(form)
+
+class DeleteProduct(LoginRequiredMixin, DeleteView):
+    
+    http_method_names = 'get', 'post'
+    model = Product
+    template_name = 'delete_confirm.html'
+    success_url = reverse_lazy('product_list')
+
+class AddCategory(LoginRequiredMixin, CreateView):
+
+    http_method_names = 'get', 'post'
+    model = Category
+    fields = '__all__'
+    template_name = 'form_category.html'
+    success_url = reverse_lazy('product_list')
